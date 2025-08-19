f76a76d357de5f9ecb8282f10981540c94d9d73a
Alchemyst <alchemyst@blockmage.dev>
2025-08-19 18:26:55 +0000

    feat: first major release v1.0.1

--- src/typed_sentinels/__init__.py
+++ src/typed_sentinels/__init__.py
@@ -28,3 +28,11 @@ from ._core import is_sentinel as is_sentinel
 from ._exceptions import InvalidHintError as InvalidHintError
 from ._exceptions import SentinelError as SentinelError
 from ._exceptions import SubscriptedTypeError as SubscriptedTypeError
+
+__all__ = (
+    'InvalidHintError',
+    'Sentinel',
+    'SentinelError',
+    'SubscriptedTypeError',
+    'is_sentinel',
+)
